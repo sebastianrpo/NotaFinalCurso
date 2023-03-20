e49c37b7a84a4d4d2e69dce214723856d7877f61
sebastianrpo <sebastian.restrepo7@udea.edu.co>
2023-03-20 15:18:55 -0500

    Corrección errores de tipeo

--- app/src/main/java/com/sebastianrpo/notafinalcurso/ui/calculanotas/CalcularNotaActivity.kt
+++ app/src/main/java/com/sebastianrpo/notafinalcurso/ui/calculanotas/CalcularNotaActivity.kt
@@ -9,7 +9,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.sebastianrpo.notafinalcurso.R
 
 class CalcularNotaActivity : AppCompatActivity() {
-    private lateinit var calcularNotabinding: ActivityCalcularNotaBinding
+    private lateinit var calcularNotaBinding: ActivityCalcularNotaBinding
     private lateinit var calcularNotaViewModel: CalcularNotaViewModel
     private var nota1: Double = 0.0
     private var nota2: Double = 0.0
@@ -17,44 +17,43 @@ class CalcularNotaActivity : AppCompatActivity() {
     private var nota4: Double = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        calcularNotabinding = ActivityCalcularNotaBinding.inflate(layoutInflater)
+        calcularNotaBinding = ActivityCalcularNotaBinding.inflate(layoutInflater)
         calcularNotaViewModel = ViewModelProvider(this)[CalcularNotaViewModel::class.java]
-        val view = calcularNotabinding.root
+        val view = calcularNotaBinding.root
         setContentView(view)
 
         val resultadoObserver = Observer<Double> { resultado ->
-            calcularNotabinding.notaFinalTextView.text = resultado.toString()
+            calcularNotaBinding.notaFinalTextView.text = resultado.toString()
         }
 
         calcularNotaViewModel.resultado.observe(this, resultadoObserver)
 
-        calcularNotabinding.calcularButton.setOnClickListener {
+        calcularNotaBinding.calcularButton.setOnClickListener {
             if (calcularNotaViewModel.validarNotasNulas(
-                    calcularNotabinding.nota1EditText.text.toString(),
-                    calcularNotabinding.nota2EditText.text.toString(),
-                    calcularNotabinding.nota3EditText.text.toString(),
-                    calcularNotabinding.nota4EditText.text.toString()
+                    calcularNotaBinding.nota1EditText.text.toString(),
+                    calcularNotaBinding.nota2EditText.text.toString(),
+                    calcularNotaBinding.nota3EditText.text.toString(),
+                    calcularNotaBinding.nota4EditText.text.toString()
                 )
             ) {
                 leerDatos()
                 if (calcularNotaViewModel.validarNotasIncorrectas(nota1, nota2, nota3, nota4)) {
                     calcularNotaViewModel.resultado(nota1, nota2, nota3, nota4)
                 } else {
-                    Toast.makeText(this, getString(R.string.nota_incorrecta), Toast.LENGTH_SHORT)
-                        .show()
-                    calcularNotabinding.notaFinalTextView.text = getString(R.string.nota_final)
+                    Toast.makeText(this, getString(R.string.nota_incorrecta), Toast.LENGTH_SHORT).show()
+                    calcularNotaBinding.notaFinalTextView.text = getString(R.string.nota_final)
                 }
             } else {
                 Toast.makeText(this, getString(R.string.nota_blanco), Toast.LENGTH_SHORT).show()
-                calcularNotabinding.notaFinalTextView.text = getString(R.string.nota_final)
+                calcularNotaBinding.notaFinalTextView.text = getString(R.string.nota_final)
             }
         }
     }
 
     private fun leerDatos() {
-        nota1 = calcularNotabinding.nota1EditText.text.toString().toDouble()
-        nota2 = calcularNotabinding.nota2EditText.text.toString().toDouble()
-        nota3 = calcularNotabinding.nota3EditText.text.toString().toDouble()
-        nota4 = calcularNotabinding.nota4EditText.text.toString().toDouble()
+        nota1 = calcularNotaBinding.nota1EditText.text.toString().toDouble()
+        nota2 = calcularNotaBinding.nota2EditText.text.toString().toDouble()
+        nota3 = calcularNotaBinding.nota3EditText.text.toString().toDouble()
+        nota4 = calcularNotaBinding.nota4EditText.text.toString().toDouble()
     }
 }
